26ca4e0a8b7829237d6d9fba349aad5f8ecdb202
Richard Holden <rholdeniii@gmail.com>
2012-06-30 00:09:42 -0600

    Add stored files to send screenshots on failure.
    StoredFile.cs comes from Lee, but I had to reverse engineer it from the IL because he forgot to commit and push it out.
    
    Signed-off-by: Richard Holden <rholdeniii@gmail.com>

---
+++ SlickSharp/StoredFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization;
+
+namespace SlickSharp
+{
+	[DataContract]
+	[ListApi("files")]
+	public class StoredFile : JsonObject<StoredFile>, IJsonObject
+	{
+		[DataMember(Name = "filename")]
+		public string FileName;
+
+		[DataMember(Name = "id")]
+		public string Id;
+
+		[DataMember(Name = "mimetype")]
+		public string Mimetype;
+
+		public StoredFile PostContent(byte[] file)
+		{
+			var uri = String.Format("{0}/{1}/{2}/{3}", ServerConfig.BaseUri, "files", Id, "content");
+			HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(new Uri(uri));
+			httpWebRequest.ContentType = "application/octet-stream";
+			httpWebRequest.Method = "POST";
+			httpWebRequest.ContentLength = file.Length;
+
+			using (Stream requestStream = httpWebRequest.GetRequestStream())
+			{
+				requestStream.Write(file, 0, file.Length);
+			}
+			using (HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse())
+			{
+				using (Stream responseStream = httpWebResponse.GetResponseStream())
+				{
+					return ReadFromStream(responseStream);
+				}
+			}
+		}
+	}
+}
