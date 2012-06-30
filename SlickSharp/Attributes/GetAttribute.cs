7ffa99b30ea414fa9c57a13af67ea5f9d0db237f
Richard Holden <rholdeniii@gmail.com>
2012-06-29 23:14:02 -0600

    Add missing directories, the move did not get handled correctly by tortoise-git.
    Signed-off-by: Richard Holden <rholdeniii@gmail.com>

---
+++ SlickSharp/Attributes/GetAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SlickSharp
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple=true, Inherited=true)]
+	class GetAttribute : Attribute
+	{
+		public GetAttribute(string apiPath, string propertyName, int index)
+		{
+			ApiPath = apiPath;
+			PropertyName = propertyName;
+			Index = index;
+		}
+
+		public string ApiPath { get; private set; }
+		public string PropertyName { get; private set; }
+		public int Index { get; private set; }
+	}
+}
