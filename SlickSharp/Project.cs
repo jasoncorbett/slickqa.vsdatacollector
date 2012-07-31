8dab295ebf4e4eabb674e3f93d118a5db25825eb
Richard Holden <rholdeniii@gmail.com>
2012-07-31 17:51:16 -0600

    Update Project equality
    Allow project objects to be equal on name if they both do not have Ids.
    
    Signed-off-by: Richard Holden <rholdeniii@gmail.com>

--- SlickSharp/Project.cs
+++ SlickSharp/Project.cs
@@ -1,17 +1,16 @@
-﻿/* Copyright 2012 AccessData Group, LLC.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+﻿// Copyright 2012 AccessData Group, LLC.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//  http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 using System;
 using System.Collections.Generic;
@@ -22,9 +21,9 @@ using SlickQA.SlickSharp.Utility;
 namespace SlickQA.SlickSharp
 {
 	[DataContract]
-	[ListApi("projects")]
-	[Get("", "Id", 0)]
-	[Get("byname", "Name", 1)]
+	[CollectionApiPath("projects")]
+	[ItemApiPath("", "Id", 0)]
+	[ItemApiPath("byname", "Name", 1)]
 	public sealed class Project : JsonObject<Project>, IJsonObject
 	{
 		[DataMember(Name = "attributes")]
@@ -93,7 +92,11 @@ namespace SlickQA.SlickSharp
 			{
 				return false;
 			}
-			return other.Id == Id;
+			if (other.Id != null && Id != null)
+			{
+				return other.Id == Id;
+			}
+			return other.Name != null && Name != null && other.Name == Name;
 		}
 
 		public override int GetHashCode()
